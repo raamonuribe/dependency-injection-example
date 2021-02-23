9a29298fbccf6dc28b55dc2bc41c7be67040d453
Ramon <ramon@ramonuribe.com>
2021-02-22 21:53:35 -0700

    Initialized Project

---
+++ src/main/java/com/ramonuribe/dependencyinjectionexample/DependencyInjectionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.dependencyinjectionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonuribe/dependencyinjectionexample/DependencyInjectionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.dependencyinjectionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
